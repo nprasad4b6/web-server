49c4819b3e7f156e80a09a7e4db342440bc1d4ff
prasad nagireddi <nprasad4b6@gmail.com>
2019-06-27 01:06:48 +0530

    some additions

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,19 @@
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageone.textcontent = 'loading..'
+    messageone.textcontent = ''
 
-fetch('')
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                 messageone.textcontent = data.error
+            } else {
+                 messageone.textcontent = data.forecast
+                 messageone.textcontent = ''
+            }
+
+        })
+    })
+
+})
